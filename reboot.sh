ca9bf223d892e6634f612ffbd05cde5ab0c3bd6d
itsRealM13C <testelek212@gmail.com>
2026-05-15 11:50:45 +0200

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
+
+luna-send -n -f luna://com.webos.settingsservice/setSystemSettings '{
+  "category": "general",
+  "settings": {
+    "defaultApps": {
+      "home": "com.webos.app.home"
+    }
+  }
+}'
+
 reboot
